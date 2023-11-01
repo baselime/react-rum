3464d1d2a73085b5c864c7282be913beded98500
Ankcorn <thomasankcorn@gmail.com>
2023-11-01 17:38:18 +0000

    remove final reference to ../../dist

--- example/app/layout.tsx
+++ example/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { BaselimeRum } from '../../dist'
+import { BaselimeRum } from '@baselime/react-rum'
 
 const inter = Inter({ subsets: ['latin'] })
 
