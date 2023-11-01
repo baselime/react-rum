1f07304ceee3fad45c09531e23badc6fb8d4f4ce
Ankcorn <thomasankcorn@gmail.com>
2023-11-01 17:36:36 +0000

    fix example app

--- example/app/user/page.tsx
+++ example/app/user/page.tsx
@@ -1,47 +1,49 @@
 'use client'
 import Image from 'next/image'
 import React from 'react'
-import { BaselimeRum, BaselimeErrorBoundary, useBaselimeRum } from '../../../dist';
+import { BaselimeErrorBoundary, useBaselimeRum } from '@baselime/react-rum';
 
 
 export default function Home() {
   const [broken, setBroken] = React.useState(false)
   return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <button onClick={() => {
-          setBroken(true)
-        }} >
-          <Image
-            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </button>
-        <BaselimeErrorBoundary fallback={<div>Oh No</div>}>
-          <BrokenComponent broken={broken} />
-        </BaselimeErrorBoundary>
-      </main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <button onClick={() => {
+        setBroken(true)
+      }} >
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </button>
+      <UserProfile />
+      <BaselimeErrorBoundary fallbackRender={(error) => <div>{error.error.message}</div>}>
+        <BrokenComponent broken={broken} />
+      </BaselimeErrorBoundary>
+    </main>
   )
 }
 
-function UserProfile({}) { 
-    const { sendEvent } = useBaselimeRum();
-    return <button onClick={() => sendEvent({})}>
-        User Profile
-    </button>
+function UserProfile({ }) {
+  const { sendEvent } = useBaselimeRum();
+  return <button onClick={() => sendEvent("User Profile Clicked", {
+    name: "John Doe",
+    email: "yeyy@clickme.com"
+  })}>
+    User Profile
+  </button>
 }
 
-function BrokenComponent({ broken }: { broken: boolean}) {
-  const stuff = {
-
+function BrokenComponent({ broken }: { broken: boolean }) {
+  if (broken) {
+    throw new Error('This application has crashed')
   }
-  
-
   return (<div>Broken
 
-    <button onClick={() => stuff.break()}></button>
+
   </div>)
 }
